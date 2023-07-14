7ec1e292075e67736830f8e974ac3364c1abf780
Ethan Waldo <ewaldo@bloombergindustry.com>
2023-07-14 09:51:10 -0400

    Use service instead of deprecated plist

--- Formula/tinyproxy-libproxy.rb
+++ Formula/tinyproxy-libproxy.rb
@@ -59,27 +59,10 @@ class TinyproxyLibproxy < Formula
     end
   end
 
-  plist_options :manual => "tinyproxy"
-
-  def plist; <<~EOS
-    <?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-    <plist version="1.0">
-      <dict>
-        <key>Label</key>
-        <string>#{plist_name}</string>
-        <key>RunAtLoad</key>
-        <true/>
-        <key>KeepAlive</key>
-        <false/>
-        <key>ProgramArguments</key>
-        <array>
-            <string>#{opt_sbin}/tinyproxy</string>
-        </array>
-        <key>WorkingDirectory</key>
-        <string>#{HOMEBREW_PREFIX}</string>
-      </dict>
-    </plist>
-    EOS
+  service do
+    run [opt_sbin/"tinyproxy"]
+    require_root true
+    keep_alive true
+    working_dir HOMEBREW_PREFIX
   end
 end
